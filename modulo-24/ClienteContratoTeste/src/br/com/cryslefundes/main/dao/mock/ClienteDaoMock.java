340bf1099c2f31bd9e2a56a85117e9b63d75a86b
Crystian Lefundes <crystian.lf@gmail.com>
2024-02-08 14:52:33 -0300

    Cria mock das interfaces de cliente e contrato

---
+++ modulo-24/ClienteContratoTeste/src/br/com/cryslefundes/main/dao/mock/ClienteDaoMock.java
@@ -0,0 +1,25 @@
+package br.com.cryslefundes.main.dao.mock;
+
+import br.com.cryslefundes.main.ICliente;
+
+public class ClienteDaoMock implements ICliente {
+    @Override
+    public String salvar() {
+        return null;
+    }
+
+    @Override
+    public String buscar() {
+        return null;
+    }
+
+    @Override
+    public String excluir() {
+        return null;
+    }
+
+    @Override
+    public String atualizar() {
+        return null;
+    }
+}
