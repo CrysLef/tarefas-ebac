6f86b472baaa09bb2830d0fa71c104bd09c0b159
crystian lefundes <crystian.lf@gmail.com>
2024-04-18 11:46:23 -0300

    Corrige documentação

--- testes/src/main/br/com/cryslefundes/Calculadora.java
+++ testes/src/main/br/com/cryslefundes/Calculadora.java
@@ -1,14 +1,14 @@
 package main.br.com.cryslefundes;
 
 /**
- * Classe que realiza operações básicas de uma calculadora, utilizando somente 2 números.
+ * Classe que realiza operações básicas de uma calculadora.
  */
 public class Calculadora {
 
     /**
      * Realiza a soma entre dois elementos.
      * @param a elemento a ser somado.
-     * @param b elemento a ser somado.
+     * @param b valor que irá ser adicionado ao elemento a.
      * @return A soma entre os elementos a e b.
      */
     public int adicionar(int a, int b) {
@@ -18,7 +18,7 @@ public class Calculadora {
     /**
      * Realiza a subtração entre dois elementos.
      * @param a elemento a ser subtraido.
-     * @param b elemento a ser subtraido.
+     * @param b valor que irá ser subtraido do elemento a.
      * @return A subtração entre os elementos a e b.
      */
     public int subtrair(int a, int b) {
@@ -28,7 +28,7 @@ public class Calculadora {
     /**
      * Realiza a multiplicação entre dois elementos.
      * @param a elemento a ser multiplicado.
-     * @param b elemento a ser multiplicado.
+     * @param b valor que irá multiplicar o elemento a.
      * @return A multiplicação entre os elementos a e b.
      */
     public int multiplicar(int a, int b) {
@@ -38,7 +38,7 @@ public class Calculadora {
     /**
      * Realiza a divisão entre dois elementos.
      * @param a elemento a ser divido.
-     * @param b elemento a ser divido.
+     * @param b valor que irá dividir o elemento a.
      * @return A divisão entre os elementos a e b.
      */
     public int dividir(int a, int b) {
